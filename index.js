bb2c088f611d972e9d32e3b7f839e10a29bad703
Miikka Virtanen <dragory.up@gmail.com>
2014-06-26 00:53:51 +0300

    Fix returned stream not ending after all target files had been handled.

--- index.js
+++ index.js
@@ -40,101 +40,79 @@ module.exports = function(targetFiles) {
 			}
 
 			resolver(mappingPatterns);
+			// Don't end the stream; new files coming from below
 		}
 	);
-	
-	targetFiles.pipe(es.map(function(targetFile, callback) {
-		// Wait for the manifest files to be parsed
-		targetFileHandlerPromise.then(function(mappingPatterns) {
-			var replacer = es.through(function(data) {
-				var strData = data.toString(),
-					i, len;
 
-				// Apply each mapping replacement to the file's contents
-				for (i = 0, len = mappingPatterns.from.length; i < len; i++) {
-					strData = strData.replace(mappingPatterns.from[i], mappingPatterns.to[i]);
-				}
+	// Some trickery to accomodate for two streams ending in async (original manifest files and targetFiles)
+	// (There has to be a better way...)
+	var totalTargetFiles = 0,
+		handled = 0,
+		hasEnded = false;
 
-				if (targetFile.isBuffer()) {
-					targetFile.contents = new Buffer(strData);
-					stream.queue(targetFile);
-				} else {
-					targetFile.contents = es.through();
-					stream.queue(targetFile);
+	function finish() {
+		if (! hasEnded || handled < totalTargetFiles) return;
+		
+		// All target files should now be handled and passed to the original stream; thus, we can end it.
+		// This also needs to be done for certain modules (such as run-sequence) to notice we've handled each file.
+		stream.end();
+	}
 
-					targetFile.contents.write(new Buffer(strData));
-					targetFile.contents.end();
-				}
+	targetFiles
+		// Keep a count of how many files we receive
+		.pipe(es.through(function(file) {
+			totalTargetFiles++;
+			this.queue(file);
+		}))
+		// Handle the files
+		.pipe(es.through(
+			function(targetFile) {
+				// Wait for the manifest files to be parsed before doing anything
+				targetFileHandlerPromise.then(function(mappingPatterns) {
+					// Vinyl .pipe() allows us to handle both stream and buffer contents
+					var replacer = es.through(function(data) {
+						var strData = data.toString(),
+							i, len;
 
-			});
+						// Apply each mapping replacement to the file's contents
+						for (i = 0, len = mappingPatterns.from.length; i < len; i++) {
+							strData = strData.replace(mappingPatterns.from[i], mappingPatterns.to[i]);
+						}
 
-			targetFile.pipe(replacer);
+						if (targetFile.isBuffer()) {
+							// For buffer files, just save the contents as a buffer
+							targetFile.contents = new Buffer(strData);
+							stream.queue(targetFile);
+						} else {
+							// For stream files, pass a new stream as the contents
+							targetFile.contents = es.through();
+							stream.queue(targetFile);
 
-			callback();
-		});
-	}));
+							targetFile.contents.write(new Buffer(strData));
+							targetFile.contents.queue(null);
+						}
+
+						// Mark this file as handled
+						handled++;
+						finish();
+					});
+
+					if (! targetFile.isNull()) {
+						targetFile.pipe(replacer);
+					} else {
+						// Ignore null files
+						handled++;
+						finish();
+					}
+				});
+			},
+
+			function() {
+				// Keep track of when we've attached the promise listener above to each of the files
+				hasEnded = true;
+				finish();
+			}
+		));
 
 	return stream;
 };
-
-/*module.exports = function(manifestFiles) {
-	// We need to load the mappings from the manifests before we can perform any replacements
-	var manifestLoadPromise = new Promise(function(resolve) {
-		var mappings = {};
-
-		manifestFiles.pipe(es.through(
-			// Load manifest files
-			function(file) {
-				var parseStream = es.through(function(data) {
-					mappings = extend(mappings, JSON.parse(data));
-				});
-
-				file.pipe(parseStream);
-			},
-
-			// Separate them into 'from' (a regex pattern) and 'to' (the replacement string)
-			function() {
-				var mappingPatterns = {
-					from: [],
-					to: []
-				};
-
-				for (var from in mappings) {
-					mappingPatterns.from.push(new RegExp(regexEscape(from), 'g'));
-					mappingPatterns.to.push(mappings[from]);
-				}
-
-				resolve(mappingPatterns);
-			}
-		));
-	});
-
-	return es.map(function(file, callback) {
-		if (file.isNull()) {
-			callback(null, file);
-			return;
-		}
-
-		manifestLoadPromise.then(function(mappingPatterns) {
-			file.pipe(es.through(
-				// Perform each replacement on the file
-				function(data) {
-					var strData = data.toString(),
-						i, len;
-
-					// Apply each mapping replacement to the file's contents
-					for (i = 0, len = mappingPatterns.from.length; i < len; i++) {
-						strData = strData.replace(mappingPatterns.from[i], mappingPatterns.to[i]);
-					}
-
-					file.contents = new Buffer(strData);
-				},
-
-				// Pass the replacement result along in the stream
-				function() {
-					callback(null, file);
-				}
-			));
-		});
-	});
-};*/
